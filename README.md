f4534ff74eb3111cc0bc4f53ba6627b258fd6a80
Silvio Paganini <paganini.silvio@bcgplatinion.com>
2021-04-09 13:25:09 +0100

    modular contracts

---
+++ README.md
@@ -0,0 +1,3 @@
+# NFT Contracts
+
+Contracts to be used with [NFT Marketplace](https://github.com/silviopaganini/nft-market)
---
+++ devops/mintPopulateGallery.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const colors = require("colors");
+const fetch = require("node-fetch");
+const { utils, ethers } = require("ethers");
+const NFTT = artifacts.require("NFTT.sol");
+
+const { SERVICE_URL } = process.env;
+
+const start = async (callback) => {
+  try {
+    const objectsToBeMinted = [];
+
+    const accounts = () =>
+      new HDWalletProvider({
+        mnemonic: process.env.KEY_MNEMONIC,
+        providerOrUrl: process.env.WALLET_PROVIDER_URL,
+      });
+
+    const FROM = ethers.utils.getAddress(accounts().getAddresses()[0]);
+
+    for (let i = 1; i < 11; i++) {
+      objectsToBeMinted.push(`Robot ${i}`);
+    }
+
+    const mintAssetsOnIPFS = await (
+      await fetch(`${SERVICE_URL}/mint`, {
+        method: "POST",
+        body: JSON.stringify({ objectsToBeMinted }),
+      })
+    ).json();
+
+    const contract = await NFTT.deployed();
+    const price = "0.5";
+
+    const priceWei = utils.parseEther(price);
+    const ipfsURLs = [];
+
+    const mintedTokens = await Promise.all(
+      mintAssetsOnIPFS.map(async (token) => {
+        ipfsURLs.push({
+          name: token.name,
+          path: token.path,
+        });
+        return await contract.mintCollectable(
+          FROM,
+          token.path,
+          token.name,
+          priceWei,
+          true,
+          {
+            from: FROM,
+          }
+        );
+      })
+    );
+
+    console.log(JSON.stringify(ipfsURLs));
+
+    callback(
+      colors.green(`⚡️ Tokens created: ${colors.white(mintedTokens.length)}`)
+    );
+  } catch (e) {
+    callback(e);
+  }
+};
+
+module.exports = start;
